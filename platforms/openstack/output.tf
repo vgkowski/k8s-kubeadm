3be4e8621fe83bc3ba157259f197d86c46a0151f
vincent gromakowski <vincent.gromakowski@gmail.com>
2018-04-03 16:45:19 +0200

    add output ct for debug

--- platforms/openstack/output.tf
+++ platforms/openstack/output.tf
@@ -9,3 +9,7 @@ output "masters-ip" {
 output "workers-ip" {
   value = "${openstack_networking_port_v2.worker.*.all_fixed_ips}"
 }
+
+output "cloud-init" {
+  value = "${module.master_cloudconfig.cloud_config}"
+}
